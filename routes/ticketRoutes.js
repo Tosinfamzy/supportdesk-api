30e89d9d5a4d15fc306354110a2c6e19cb4aa8d6
Tosinfamzy <famurewatosin@gmail.com>
2022-07-08 08:49:45 +0100

    CRUD ticket functionality

---
+++ routes/ticketRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const {
+  getTickets,
+  createTicket,
+  getTicket,
+  deleteTicket,
+  updateTicket,
+} = require("../controllers/ticketController");
+const router = express.Router();
+
+const { protect } = require("../middleware/auth");
+
+router.route("/").get(protect, getTickets).post(protect, createTicket);
+router
+  .route("/:id")
+  .get(protect, getTicket)
+  .delete(protect, deleteTicket)
+  .put(protect, updateTicket);
+
+module.exports = router;
