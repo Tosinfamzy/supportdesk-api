5914af371e8fa77e13522493e4aaa728dc893c53
Tosinfamzy <famurewatosin@gmail.com>
2022-07-08 08:49:30 +0100

    CRUD ticket functionality

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/users/", require("./routes/userRoutes"));
+app.use("/api/tickets", require("./routes/ticketRoutes"));
 app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`server running on ${PORT}`));
